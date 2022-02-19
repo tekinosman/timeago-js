6ecb0d9468c946d67675e8a2405a8ef0616fd8c1
tekinosman <100043659+tekinosman@users.noreply.github.com>
2022-02-19 19:52:52 +0100

    Create languages.js

---
+++ languages.js
@@ -0,0 +1,11 @@
+const it_IT = [
+  "adesso",   			 "secondi fa",
+  "un minuto fa", 		 "minuti fa",
+  "un'ora fa",   		 "ore fa",
+  "un giorno fa",  	 	 "giorni fa",
+  "una settimana fa",    "settimane fa",
+  "un mese fa",  	 	 "mesi fa",
+  "un anno fa", 		 "anni fa"
+];
+
+export {it_IT}
